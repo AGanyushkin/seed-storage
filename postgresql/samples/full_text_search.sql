ea4168572401cafbef98e9576953224cdcfeb5de
Andrey Ganyushkin <andrey@ganyushkin.ru>
2023-01-03 01:10:22 +0300

    checkstyle

--- postgresql/samples/full_text_search.sql
+++ postgresql/samples/full_text_search.sql
@@ -47,9 +47,11 @@ WHERE
 SELECT *
 FROM plant_name
 WHERE
-    (language_id = 1 AND to_tsvector('russian', "text") @@ plainto_tsquery('russian', 'Ель ''Хупс'''))
+    (language_id = 1 AND
+     to_tsvector('russian', "text") @@ plainto_tsquery('russian', 'Ель ''Хупс'''))
     OR
-    ((language_id = 2 OR language_id = 3) AND to_tsvector('english', "text") @@ plainto_tsquery('english', 'Ель ''Хупс'''))
+    ((language_id = 2 OR language_id = 3) AND
+     to_tsvector('english', "text") @@ plainto_tsquery('english', 'Ель ''Хупс'''))
 ;
 
 --- examples
